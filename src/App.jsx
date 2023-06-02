0929fda58451f28bd5128651a7bc286bb5a48092
unknown <mcmahanconner@gmail.com>
2023-06-02 16:32:11 -0400

    basic layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
 import React, { useState } from 'react'
-import './App.css'
+import './App.scss'
 
 function App() {
 
 
   return (
-<div className="app">Hello, world</div>
+<div className="app">
+  <div className="builder">
+    <div className="builder-item occupation">
+      Occupation
+    </div>
+    <div className="builder-item traits">
+      <div className="traits-item top">Traits</div>
+      <div className="traits-item bottom">Negative Traits</div>
+    </div>
+    <div className="builder-item chosen">
+      <div className="chosen-item top">Chosen Traits</div>
+      <div className="chosen-item bottom">Major Skills</div>
+    </div>
+  </div>
+</div>
   )
 }
 
