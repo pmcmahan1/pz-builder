c929feca274250d8d7becbdb4d088c98b0aad34a
unknown <mcmahanconner@gmail.com>
2023-06-07 02:43:35 -0400

    Fix image links

--- src/badtraits.js
+++ src/badtraits.js
@@ -5,244 +5,244 @@ export default [
         value: 1,
         index: 1,
         desc: "Drives very slow.",
-        icon: "/badtraits/sundaydriver.png"
+        icon: "/pz-builder/badtraits/sundaydriver.png"
     }, 
     {
         name: "All Thumbs",
         value: 2,
         index: 2,
         desc: "Transfers inventory items slowly.",
-        icon: "/badtraits/allthumbs.png"
+        icon: "/pz-builder/badtraits/allthumbs.png"
     }, 
     {
         name: "Clumsy",
         value: 2,
         index: 3,
         desc: "Makes more noise when moving.",
-        icon: "/badtraits/clumsy.png"
+        icon: "/pz-builder/badtraits/clumsy.png"
     }, 
     {
         name: "Cowardly",
         value: 2,
         index: 4,
         desc: "Especially prone to becoming panicked.",
-        icon: "/badtraits/cowardly.png"
+        icon: "/pz-builder/badtraits/cowardly.png"
     }, 
     {
         name: "Short Sighted",
         value: 2,
         index: 5,
         desc: "Smaller view distance. Slower visibility fade.",
-        icon: "/badtraits/shortsighted.png"
+        icon: "/pz-builder/badtraits/shortsighted.png"
     }, 
     {
         name: "Slow Reader",
         value: 2,
         index: 6,
         desc: "Takes longer to read books.",
-        icon: "/badtraits/slowreader.png"
+        icon: "/pz-builder/badtraits/slowreader.png"
     }, 
     {
         name: "Weak Stomach",
         value: 3,
         index: 7,
         desc: "Higher chance to have food illness.",
-        icon: "/badtraits/weakstomach.png"
+        icon: "/pz-builder/badtraits/weakstomach.png"
     }, 
     {
         name: "Agoraphobic",
         value: 4,
         index: 8,
         desc: "Gets panicked when outdoors.",
-        icon: "/badtraits/agoraphobic.png"
+        icon: "/pz-builder/badtraits/agoraphobic.png"
     }, 
     {
         name: "Claustrophobic",
         value: 4,
         index: 9,
         desc: "Gets panicked when indoors.",
-        icon: "/badtraits/claustrophobic.png"
+        icon: "/pz-builder/badtraits/claustrophobic.png"
     }, 
     {
         name: "Conspicuous",
         value: 4,
         index: 10,
         desc: "More likely to be spotted by zombies.",
-        icon: "/badtraits/conspicuous.png"
+        icon: "/pz-builder/badtraits/conspicuous.png"
     }, 
     {
         name: "Disorganized",
         value: 4,
         index: 11,
         desc: "Decreased container inventory capacity.",
-        icon: "/badtraits/disorganized.png"
+        icon: "/pz-builder/badtraits/disorganized.png"
     }, 
     {
         name: "Hard of Hearing",
         value: 4,
         index: 12,
         desc: "Smaller perception radius. Smaller hearing range.",
-        icon: "/badtraits/hardofhearing.png"
+        icon: "/pz-builder/badtraits/hardofhearing.png"
     }, 
     {
         name: "Hearty Appetite",
         value: 4,
         index: 13,
         desc: "Needs to eat more regularly.",
-        icon: "/badtraits/heartyappetite.png"
+        icon: "/pz-builder/badtraits/heartyappetite.png"
     }, 
     {
         name: "Pacifist",
         value: 4,
         index: 14,
         desc: "Less effective with weapons.",
-        icon: "/badtraits/pacifist.png"
+        icon: "/pz-builder/badtraits/pacifist.png"
     }, 
     {
         name: "Prone to Illness",
         value: 4,
         index: 15,
         desc: "More prone to disease. Faster rate of zombification.",
-        icon: "/badtraits/pronetoillness.png"
+        icon: "/pz-builder/badtraits/pronetoillness.png"
     }, 
     {
         name: "Sleepyhead",
         value: 4,
         index: 16,
         desc: "Needs more sleep.",
-        icon: "/badtraits/sleepyhead.png"
+        icon: "/pz-builder/badtraits/sleepyhead.png"
     }, 
     {
         name: "Smoker",
         value: 4,
         index: 17,
         desc: "Stress and unhappiness decrease after smoking tobacco. Unhappiness rises when tobacco is not smoked.",
-        icon: "/badtraits/smoker.png"
+        icon: "/pz-builder/badtraits/smoker.png"
     }, 
     {
         name: "Unlucky",
         value: 4,
         index: 18,
         desc: "What could go wrong for you, often does.",
-        icon: "/badtraits/unlucky.png"
+        icon: "/pz-builder/badtraits/unlucky.png"
     }, 
     {
         name: "Asthmatic",
         value: 5,
         index: 19,
         desc: "Faster endurance loss.",
-        icon: "/badtraits/asthmatic.png"
+        icon: "/pz-builder/badtraits/asthmatic.png"
     }, 
     {
         name: "Fear of Blood",
         value: 5,
         index: 20,
         desc: "Panic when performing first aid on self, cannot perform first aid on others, get stressed when bloody.",
-        icon: "/badtraits/fearofblood.png"
+        icon: "/pz-builder/badtraits/fearofblood.png"
     }, 
     {
         name: "Feeble",
         value: 6,
         index: 21,
         desc: "Less knockback from melee weapons. Decreased carrying weight. -2 Strength.",
-        icon: "/badtraits/feeble.png"
+        icon: "/pz-builder/badtraits/feeble.png"
     }, 
     {
         name: "High Thirst",
         value: 6,
         index: 22,
         desc: "Needs more water to survive.",
-        icon: "/badtraits/highthirst.png"
+        icon: "/pz-builder/badtraits/highthirst.png"
     },
     {
         name: "Out of Shape",
         value: 6,
         index: 23,
         desc: "Low endurance, low endurance regeneration. -2 Fitness.",
-        icon: "/badtraits/outofshape.png"
+        icon: "/pz-builder/badtraits/outofshape.png"
     },
     {
         name: "Overweight",
         value: 6,
         index: 24,
         desc: "Reduced running speed, low endurance and prone to injury. -1 Fitness.",
-        icon: "/badtraits/overweight.png"
+        icon: "/pz-builder/badtraits/overweight.png"
     },
     {
         name: "Restless Sleeper",
         value: 6,
         index: 25,
         desc: "Slow loss of tiredness while sleeping.",
-        icon: "/badtraits/restlesssleeper.png"
+        icon: "/pz-builder/badtraits/restlesssleeper.png"
     },
     {
         name: "Slow Healer",
         value: 6,
         index: 26,
         desc: "Recovers slowly from injuries and illness.",
-        icon: "/badtraits/slowhealer.png"
+        icon: "/pz-builder/badtraits/slowhealer.png"
     },
     {
         name: "Slow Learner",
         value: 6,
         index: 27,
         desc: "Decreased XP gains.",
-        icon: "/badtraits/slowlearner.png"
+        icon: "/pz-builder/badtraits/slowlearner.png"
     },
     {
         name: "Underweight",
         value: 6,
         index: 28,
         desc: "Low strength, low endurance and prone to injury. -1 Fitness.",
-        icon: "/badtraits/underweight.png"
+        icon: "/pz-builder/badtraits/underweight.png"
     },
     {
         name: "Illiterate",
         value: 8,
         index: 29,
         desc: "Cannot read books.",
-        icon: "/badtraits/illiterate.png"
+        icon: "/pz-builder/badtraits/illiterate.png"
     },
     {
         name: "Thin-skinned",
         value: 8,
         index: 30,
         desc: "Increased chance of scratches, lacerations, or bites breaking the skin.",
-        icon: "/badtraits/thinskinned.png"
+        icon: "/pz-builder/badtraits/thinskinned.png"
     },
     {
         name: "Obese",
         value: 10,
         index: 31,
         desc: "Reduced running speed, very low endurance and prone to injury. -2 Fitness.",
-        icon: "/badtraits/obese.png"
+        icon: "/pz-builder/badtraits/obese.png"
     },
     {
         name: "Unfit",
         value: 10,
         index: 32,
         desc: "Very low endurance, very low endurance regeneration. -4 Fitness.",
-        icon: "/badtraits/unfit.png"
+        icon: "/pz-builder/badtraits/unfit.png"
     },
     {
         name: "Very Underweight",
         value: 10,
         index: 33,
         desc: "Very low strength, very low endurance and prone to injury.",
-        icon: "/badtraits/veryunderweight.png"
+        icon: "/pz-builder/badtraits/veryunderweight.png"
     },
     {
         name: "Weak",
         value: 10,
         index: 34,
         desc: "Less knockback from melee weapons. Decreased carrying weight. -5 Strength.",
-        icon: "/badtraits/weak.png"
+        icon: "/pz-builder/badtraits/weak.png"
     },
     {
         name: "Deaf",
         value: 12,
         index: 35,
         desc: "Can't hear sound.",
-        icon: "/badtraits/deaf.png"
+        icon: "/pz-builder/badtraits/deaf.png"
     },
 ]
--- src/goodtraits.js
+++ src/goodtraits.js
@@ -12,49 +12,49 @@ export default [
         value: -2,
         index: 2,
         desc: "Better vision at night.",
-        icon: "/goodtraits/catseyes.png"
+        icon: "/pz-builder/goodtraits/catseyes.png"
     }, 
     {
         name: "Dextrous",
         value: -2,
         index: 3,
         desc: "Transfers inventory items quickly.",
-        icon: "/goodtraits/dextrous.png"
+        icon: "/pz-builder/goodtraits/dextrous.png"
     }, 
     {
         name: "Fast Reader",
         value: -2,
         index: 4,
         desc: "Takes less time to read books.",
-        icon: "/goodtraits/fastreader.png"
+        icon: "/pz-builder/goodtraits/fastreader.png"
     }, 
     {
         name: "Outdoorsman",
         value: -2,
         index: 5,
         desc: "Not affected by harsh weather conditions.",
-        icon: "/goodtraits/outdoorsman.png"
+        icon: "/pz-builder/goodtraits/outdoorsman.png"
     }, 
     {
         name: "Wakeful",
         value: -2,
         index: 6,
         desc: "Needs less sleep.",
-        icon: "/goodtraits/wakeful.png"
+        icon: "/pz-builder/goodtraits/wakeful.png"
     }, 
     {
         name: "Iron Gut",
         value: -3,
         index: 7,
         desc: "Less chance to have food illness.",
-        icon: "/goodtraits/irongut.png"
+        icon: "/pz-builder/goodtraits/irongut.png"
     }, 
     {
         name: "Angler",
         value: -4,
         index: 8,
         desc: "+1 Fishing.",
-        icon: "/goodtraits/angler.png",
+        icon: "/pz-builder/goodtraits/angler.png",
         fishing: 1,
     }, 
     {
@@ -62,7 +62,7 @@ export default [
         value: -4,
         index: 9,
         desc: "+1 Long Blunt.",
-        icon: "/goodtraits/baseballplayer.png",
+        icon: "/pz-builder/goodtraits/baseballplayer.png",
         fishing: 12,
     }, 
     {
@@ -70,56 +70,56 @@ export default [
         value: -4,
         index: 10,
         desc: "Less prone to become panicked.",
-        icon: "/goodtraits/brave.png"
+        icon: "/pz-builder/goodtraits/brave.png"
     }, 
     {
         name: "First Aider",
         value: -4,
         index: 11,
         desc: "+1 First Aid.",
-        icon: "/goodtraits/firstaider.png"
+        icon: "/pz-builder/goodtraits/firstaider.png"
     }, 
     {
         name: "Gardener",
         value: -4,
         index: 12,
         desc: "+1 Farming.",
-        icon: "/goodtraits/gardener.png"
+        icon: "/pz-builder/goodtraits/gardener.png"
     }, 
     {
         name: "Graceful",
         value: -4,
         index: 13,
         desc: "Makes less noise when moving.",
-        icon: "/goodtraits/graceful.png"
+        icon: "/pz-builder/goodtraits/graceful.png"
     }, 
     {
         name: "Inconspicuous",
         value: -4,
         index: 14,
         desc: "Less likely to be spotted by zombies.",
-        icon: "/goodtraits/inconspicuous.png"
+        icon: "/pz-builder/goodtraits/inconspicuous.png"
     }, 
     {
         name: "Light Eater",
         value: -4,
         index: 15,
         desc: "Needs to eat less regularly.",
-        icon: "/goodtraits/lighteater.png"
+        icon: "/pz-builder/goodtraits/lighteater.png"
     },
     {
         name: "Lucky",
         value: -4,
         index: 16,
         desc: "Sometimes, things just go your way.",
-        icon: "/goodtraits/lucky.png"
+        icon: "/pz-builder/goodtraits/lucky.png"
     },  
     {
         name: "Nutritionist",
         value: -4,
         index: 17,
         desc: "Can see the nutritional values of any food.",
-        icon: "/goodtraits/nutritionist.png"
+        icon: "/pz-builder/goodtraits/nutritionist.png"
     }, 
     
     {
@@ -127,167 +127,167 @@ export default [
         value: -4,
         index: 18,
         desc: "Less prone to disease. Slower rates of zombification.",
-        icon: "/goodtraits/resilient.png"
+        icon: "/pz-builder/goodtraits/resilient.png"
     }, 
     {
         name: "Runner",
         value: -4,
         index: 19,
         desc: "+1 Sprinting.",
-        icon: "/goodtraits/runner.png"
+        icon: "/pz-builder/goodtraits/runner.png"
     }, 
     {
         name: "Sewer",
         value: -4,
         index: 20,
         desc: "+1 Tailoring.",
-        icon: "/goodtraits/sewer.png"
+        icon: "/pz-builder/goodtraits/sewer.png"
     }, 
     {
         name: "Amateur Mechanic",
         value: -5,
         index: 21,
         desc: "Has a detailed knowledge of common and commercial vehicle models, and repairs. +1 Mechanics.",  
-        icon: "/goodtraits/amateurmechanic.png"
+        icon: "/pz-builder/goodtraits/amateurmechanic.png"
     }, 
     {
         name: "Gymnast",
         value: -5,
         index: 22,
         desc: "+1 Lightfooted. +1 Nimble.",
-        icon: "/goodtraits/gymnast.png"
+        icon: "/pz-builder/goodtraits/gymnast.png"
     }, 
     {
         name: "Brawler",
         value: -6,
         index: 23,
         desc: "+1 Axe. +1 Long Blunt.",
-        icon: "/goodtraits/brawler.png"
+        icon: "/pz-builder/goodtraits/brawler.png"
     }, 
     {
         name: "Cook",
         value: -6,
         index: 24,
         desc: "Knows cooking recipes. +2 Cooking.",
-        icon: "/goodtraits/cook.png"
+        icon: "/pz-builder/goodtraits/cook.png"
     }, 
     {
         name: "Eagle Eyed",
         value: -6,
         index: 25,
         desc: "Faster visibility fade. Higher visibility arc.",
-        icon: "/goodtraits/eagleeyed.png"
+        icon: "/pz-builder/goodtraits/eagleeyed.png"
     }, 
     {
         name: "Fast Healer",
         value: -6,
         index: 26,
         desc: "Recovers quickly from injuries and illness.",
-        icon: "/goodtraits/fasthealer.png"
+        icon: "/pz-builder/goodtraits/fasthealer.png"
     }, 
     {
         name: "Fast Learner",
         value: -6,
         index: 27,
         desc: "Increased XP gains.",
-        icon: "/goodtraits/fastlearner.png"
+        icon: "/pz-builder/goodtraits/fastlearner.png"
     }, 
     {
         name: "Fit",
         value: -6,
         index: 28,
         desc: "Fit. +2 Fitness.",
-        icon: "/goodtraits/fit.png"
+        icon: "/pz-builder/goodtraits/fit.png"
     }, 
     {
         name: "Former Scout",
         value: -6,
         index: 29,
         desc: "+1 First Aid. +1 Foraging.",
-        icon: "/goodtraits/formerscout.png"
+        icon: "/pz-builder/goodtraits/formerscout.png"
     }, 
     {
         name: "Herbalist",
         value: -6,
         index: 30,
         desc: "Can find medicinal herbs and craft medicines and poultices from them.",
-        icon: "/goodtraits/herbalist.png"
+        icon: "/pz-builder/goodtraits/herbalist.png"
     }, 
     {
         name: "Hiker",
         value: -6,
         index: 31,
         desc: "+1 Foraging. +1 Trapping.",
-        icon: "/goodtraits/hiker.png"
+        icon: "/pz-builder/goodtraits/hiker.png"
     }, 
     {
         name: "Keen Hearing",
         value: -6,
         index: 32,
         desc: "Larger perception radius.",
-        icon: "/goodtraits/keenhearing.png"
+        icon: "/pz-builder/goodtraits/keenhearing.png"
     }, 
     {
         name: "Low Thirst",
         value: -6,
         index: 33,
         desc: "Needs less water to survive.",
-        icon: "/goodtraits/lowthirst.png"
+        icon: "/pz-builder/goodtraits/lowthirst.png"
     }, 
     {
         name: "Organized",
         value: -6,
         index: 34,
         desc: "Increased container inventory capacity.",
-        icon: "/goodtraits/organized.png"
+        icon: "/pz-builder/goodtraits/organized.png"
     }, 
     {
         name: "Stout",
         value: -6,
         index: 35,
         desc: "Extra knockback from melee weapons. Increased carrying weight. +2 Strength.",
-        icon: "/goodtraits/stout.png"
+        icon: "/pz-builder/goodtraits/stout.png"
     }, 
     {
         name: "Adrenaline Junkie",
         value: -8,
         index: 36,
         desc: "Moves faster when highly panicked.",
-        icon: "/goodtraits/adrenalinejunkie.png"
+        icon: "/pz-builder/goodtraits/adrenalinejunkie.png"
     }, 
     {
         name: "Handy",
         value: -8,
         index: 37,
         desc: "Faster and stronger constructions. +1 Carpentry. +1 Maintenance.",
-        icon: "/goodtraits/handy.png"
+        icon: "/pz-builder/goodtraits/handy.png"
     }, 
     {
         name: "Hunter",
         value: -8,
         index: 38,
         desc: "+1 Aiming. +1 Short Blade. +1 Sneaking. +1 Trapping.",
-        icon: "/goodtraits/hunter.png"
+        icon: "/pz-builder/goodtraits/hunter.png"
     }, 
     {
         name: "Thick Skinned",
         value: -8,
         index: 39,
         desc: "Less chance of scratches or bites breaking the skin.",
-        icon: "/goodtraits/thickskinned.png"
+        icon: "/pz-builder/goodtraits/thickskinned.png"
     }, 
     {
         name: "Athletic",
         value: -10,
         index: 40,
         desc: "Faster running speed. Can run for longer without tiring. +4 Fitness.",
-        icon: "/goodtraits/athletic.png"
+        icon: "/pz-builder/goodtraits/athletic.png"
     }, 
     {
         name: "Strong",
         value: -10,
         index: 41,
         desc: "Extra knockback from melee weapons. Increased carrying weight. +4 Strength.",
-        icon: "/goodtraits/strong.png"
+        icon: "/pz-builder/goodtraits/strong.png"
     }, 
 ]
